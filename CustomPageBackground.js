2fc09df916a8ff3e2c42706b25e6ca6483716629
Gakuto1112 <gakuto131112@gmail.com>
2022-04-23 20:47:23 +0900

    規定サイトのヘッダーとフッターも透明化

--- CustomPageBackground.js
+++ CustomPageBackground.js
@@ -12,10 +12,26 @@ chrome.storage.local.get(["apply_sites"], (siteResult) => {
 			if(backgroundResult.images.length != 0) new BackgroundImageInjector("window", backgroundResult.images[Math.floor(Math.random() * backgroundResult.images.length)], backgroundResult.style.justify_method, backgroundResult.style.opacity, backgroundResult.style.border_blur);
 		});
 		if(location.href.startsWith("https://www.google.com/search")) {
+			const searchForm = document.getElementById("searchform");
+			const header = Array.from(document.getElementsByClassName("sfbg"));
+			const observer = new MutationObserver(() => {
+				if(searchForm.classList.contains("minidiv")) header.forEach((element) => element.style.backgroundColor = "");
+				else header.forEach((element) => element.style.backgroundColor = "transparent");
+			});
+			observer.observe(searchForm, {
+				attributes: true,
+				attributeFilter: ["class"]
+			});
+			makeTransparentByClassName("sfbg");
 			makeTransparentById("pTwnEc");
 			makeTransparentById("appbar");
+			makeTransparentByClassName("GHDvEf");
+			makeTransparentByClassName("f6F9Be");
 		}
-		else if(location.href.startsWith("https://www.bing.com/search")) Array.from(document.getElementById("b_results").children).forEach((child) => child.style.backgroundColor = "transparent");
+		else if(location.href.startsWith("https://www.bing.com/search")) {
+			Array.from(document.getElementById("b_results").children).forEach((child) => child.style.backgroundColor = "transparent");
+			makeTransparentByClassName("b_footer");
+		}
 		else if(location.href.startsWith("https://search.yahoo.co.jp/search")) {
 			makeTransparentById("header");
 			makeTransparentById("contents");
