bdb800ede062f5e3eacdf9e7c3a7a8a979f781bf
Gakuto1112 <gakuto131112@gmail.com>
2022-04-23 23:03:33 +0900

    Google検索での透明部分を増加

--- CustomPageBackground.js
+++ CustomPageBackground.js
@@ -1,9 +1,11 @@
 function makeTransparentById(id) {
-	document.getElementById(id).style.backgroundColor = "transparent";
+	const targetElement = document.getElementById(id);
+	if(targetElement) targetElement.style.backgroundColor = "transparent";
 }
 
 function makeTransparentByClassName(className) {
-	Array.from(document.getElementsByClassName(className)).forEach((element) => element.style.backgroundColor = "transparent");
+	const array = Array.from(document.getElementsByClassName(className));
+	if(array.length >= 1) Array.from(document.getElementsByClassName(className)).forEach((element) => element.style.backgroundColor = "transparent");
 }
 
 chrome.storage.local.get(["apply_sites"], (siteResult) => {
@@ -12,19 +14,37 @@ chrome.storage.local.get(["apply_sites"], (siteResult) => {
 			if(backgroundResult.images.length != 0) new BackgroundImageInjector("window", backgroundResult.images[Math.floor(Math.random() * backgroundResult.images.length)], backgroundResult.style.justify_method, backgroundResult.style.opacity, backgroundResult.style.border_blur);
 		});
 		if(location.href.startsWith("https://www.google.com/search")) {
-			const searchForm = document.getElementById("searchform");
 			const header = Array.from(document.getElementsByClassName("sfbg"));
-			const observer = new MutationObserver(() => {
-				if(searchForm.classList.contains("minidiv")) header.forEach((element) => element.style.backgroundColor = "");
-				else header.forEach((element) => element.style.backgroundColor = "transparent");
-			});
-			observer.observe(searchForm, {
-				attributes: true,
-				attributeFilter: ["class"]
-			});
+			const searchForm = document.getElementById("searchform");
+			if(searchForm) {
+				const observer = new MutationObserver(() => {
+					if(searchForm.classList.contains("minidiv")) header.forEach((element) => element.style.backgroundColor = "");
+					else header.forEach((element) => element.style.backgroundColor = "transparent");
+				});
+				observer.observe(searchForm, {
+					attributes: true,
+					attributeFilter: ["class"]
+				});				
+			}
+			const kO001e = document.getElementById("kO001e");
+			if(kO001e) {
+				const observer = new MutationObserver(() => {
+					if(kO001e.classList.contains("DU1Mzb")) kO001e.style.backgroundColor = "";
+					else kO001e.style.backgroundColor = "transparent";
+				});
+				observer.observe(kO001e, {
+					attributes: true,
+					attributeFilter: ["class"]
+				});
+			}
 			makeTransparentByClassName("sfbg");
+			makeTransparentByClassName("s8GCU");
+			makeTransparentByClassName("jZWadf")
+			makeTransparentById("kO001e");
 			makeTransparentById("pTwnEc");
 			makeTransparentById("appbar");
+			makeTransparentByClassName("dG2XIf");
+			makeTransparentByClassName("yyjhs");
 			makeTransparentByClassName("GHDvEf");
 			makeTransparentByClassName("f6F9Be");
 		}
