6e37851712fb089c18c322dccec750e4462b0351
Gakuto1112 <gakuto131112@gmail.com>
2023-03-23 00:04:04 +0900

    bingショッピングの要素透明化

---
+++ site_css/bing_shopping.js
@@ -0,0 +1,5 @@
+//背景の白色がID指定かつimportantされているので、挿入cssでは背景を消せない。そこで、スクリプトを用いて無理矢理背景を消す。
+document.getElementById("resultsContainer").style.setProperty("background-color", "transparent", "important");
+Array.prototype.forEach.call(document.getElementsByClassName("br-card"), (element) => {
+	element.style.setProperty("border", "none", "important");
+});
