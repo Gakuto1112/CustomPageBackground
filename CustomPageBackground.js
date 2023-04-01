63c8c1d17a2bf91199df80d93fcb50361bca812c
Gakuto1112 <gakuto131112@gmail.com>
2023-04-01 20:24:15 +0900

    google検索でサイドパネル非表示のオプションが機能するように

--- CustomPageBackground.js
+++ CustomPageBackground.js
@@ -1,3 +1,4 @@
+//画像の挿入
 Promise.all([
 	import(chrome.runtime.getURL("BackgroundImageInjector.js")),
 	import(chrome.runtime.getURL("DataStructureUpdater.js")),
@@ -10,4 +11,31 @@ Promise.all([
 			}
 		});
 	});
+});
+
+//サイドバーの非表示
+chrome.storage.local.get("site_config", (siteConfig) => {
+	if(siteConfig.site_config.general.hide_side_panel) {
+		["TQc1id"].forEach((className) => {
+			Array.from(document.getElementsByClassName(className)).forEach((element) => {
+				const sidePanelArea = document.createElement("div");
+				sidePanelArea.classList.add("background_image_injector_side_panel_area");
+				element.classList.forEach((sidePanelClass) => {
+					sidePanelArea.classList.add(sidePanelClass);
+					element.classList.remove(sidePanelClass);
+				});
+				element.classList.add("background_image_injector_hidden_base", "background_image_injector_hidden");
+				const showSidePanelButton = document.createElement("button");
+				showSidePanelButton.innerText = "サイドパネル再表示";
+				showSidePanelButton.classList.add("background_image_injector_show_side_panel_button");
+				showSidePanelButton.addEventListener("click", () => {
+					element.classList.remove("background_image_injector_hidden");
+					showSidePanelButton.remove();
+				}, {once: true});
+				sidePanelArea.appendChild(showSidePanelButton);
+				element.parentNode.appendChild(sidePanelArea);
+				sidePanelArea.appendChild(element);
+			});
+		});
+	}
 });
